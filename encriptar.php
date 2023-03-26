ebbd6f0f2df2ce82566fc63c5a568fb2ea202b8b
Alan Parra <38467231+Alan250812GPR@users.noreply.github.com>
2023-03-25 21:26:10 -0600

    Update encriptar.php

--- encriptar.php
+++ encriptar.php
@@ -3,7 +3,7 @@
 function encriptar($texto){
     $key = "NAyT78HkUFKvLi81njDpX2)EB71JjNV(KwFWU2TB#";
     $hash = md5($key);
-    $cifrado = openssl_encrypt($texto,'AES-256-CBC',$hash,OPENSSL_RAW_DATA,"SmartRaccon");
+    $cifrado = openssl_encrypt($texto,'AES-256-CBC',$hash,OPENSSL_RAW_DATA,"Some");
     $codificado = base64_encode($cifrado);
 
     return $codificado;
